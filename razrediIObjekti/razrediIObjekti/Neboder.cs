0c07a54f1497b9a3e7631a93ac362d538b1ec30d
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-12-19 11:04:34 +0100

    Add files via upload

---
+++ razrediIObjekti/razrediIObjekti/Neboder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediIObjekti
+{
+    internal class Neboder
+    {
+        private float visina = 200;
+        private int katovi = 100;
+        int kat = 1;
+        int visi = 10;
+       
+
+        public Neboder()
+        {
+        }
+
+       
+        public void setvisina(float visina)
+        {
+            this.visina = visina;
+        }
+        public void setkatovi(int katovi)
+        {
+            this.katovi = katovi;
+        }
+
+
+        public float getvisina()
+        {
+            return this.visina + visi;
+        }
+
+        public int getkatovi()
+        {
+            return this.katovi + kat;
+        }
+
+
+        public override string ToString()
+        {
+            string ispis = "Visina Nebodera: " + this.getvisina() + " m" + Environment.NewLine + " Katovi Nebodera iznose: " + this.getkatovi() + " m" Environment.NewLine + " Prosjek je: " + this.getvisina()/this.getkatovi() + " m";
+            return ispis;
+        }
+
+        public Neboder(int visina, int katovi)
+        {
+            this.visina = visina;
+            this.katovi = katovi;
+        }
+
+
+        
+
+    }
+}
---
+++ razrediIObjekti/razrediIObjekti/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediIObjekti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Neboder nebo = new Neboder();
+
+            Console.WriteLine(nebo.ToString());
+
+            Console.ReadKey();
+
+        }
+    }
+}
